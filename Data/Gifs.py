ef577a95cc47d84e7613cf6ffb7db60fe119a910
EVILFOXy <60402948+EVILFOXy@users.noreply.github.com>
2020-03-02 17:08:57 +0700

    Add files via upload

---
+++ Data/Gifs.py
@@ -0,0 +1,21 @@
+import random
+
+Sexy_gifs = [
+    'https://discordemoji.com/assets/emoji/7857_SexyLicking.gif',
+    'https://discordemoji.com/assets/emoji/5794_BootyShake.gif',
+    'https://discordemoji.com/assets/emoji/1083_TranceBoobies.gif',
+    'https://discordemoji.com/assets/emoji/9607_Booty.gif',
+    'https://discordemoji.com/assets/emoji/7878_PrinzEugen_sucking.gif',
+    'https://discordemoji.com/assets/emoji/9703_mmmDasNice.gif',
+    'https://discordemoji.com/assets/emoji/7345_nursebelle.gif',
+    'https://discordemoji.com/assets/emoji/4580_bellesbanana.gif',
+    'https://discordemoji.com/assets/emoji/7198_BelleXxX.gif',
+    'https://discordemoji.com/assets/emoji/9457_belledwink.gif',
+    'https://discordemoji.com/assets/emoji/4278_Ahegao.gif',
+    'https://discordemoji.com/assets/emoji/8290_intslMeiOppai.gif'
+]
+
+
+def r_gif():
+    choice = random.choice(Sexy_gifs)
+    return choice
---
+++ Data/def_random.py
@@ -0,0 +1,6 @@
+import random
+
+
+def random_num(arg_1, arg_2):
+	choice = random.randint(arg_1, arg_2)
+	return choice
